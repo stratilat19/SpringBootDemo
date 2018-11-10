05bac71e16bbf8c36ff90438492a7f36fc4fda15
Mikhail.Kurnikov <stratilat19@yahoo.com>
2018-11-10 18:57:43 +0100

    method POST added

--- src/main/java/com/learnSpring/controllers/MvcController.java
+++ src/main/java/com/learnSpring/controllers/MvcController.java
@@ -3,6 +3,7 @@ package com.learnSpring.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -23,4 +24,13 @@ public class MvcController {
         model.addAttribute("os", os);
         return "contacts";
     }
+
+    @PostMapping
+    public String feedback(@RequestParam String name,
+                           @RequestParam String email,
+                           Model model){
+        System.out.println("FEEDBACK name="+name+" email="+email);
+        return "contacts";
+    }
+
 }
