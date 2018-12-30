9f6a278f9b855d24b44af3fe54aace210dee7e51
Mikhail.Kurnikov <stratilat19@yahoo.com>
2018-12-30 15:57:27 +0100

    pages fragmented pricelistform added

--- src/main/java/com/learnSpring/controllers/MvcController.java
+++ src/main/java/com/learnSpring/controllers/MvcController.java
@@ -3,7 +3,6 @@ package com.learnSpring.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -25,6 +24,13 @@ public class MvcController {
         return "contacts";
     }
 
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
 
-
+    @GetMapping("/pricelist")
+    public String pricelist() {
+        return "pricelist";
+    }
 }
